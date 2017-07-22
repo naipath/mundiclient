14b1b715ebe6d93e939aa459e88da3e88d6ecf55
tvs <teije-vs@hotmail.com>
2017-07-22 15:44:09 +0200

    Refactored a bit

--- systemtimedate.go
+++ systemtimedate.go
@@ -4,20 +4,27 @@ import "time"
 
 const (
 	getSystemTime = 0xA8
+	setSystemTime = 0xAA
 	getSystemDate = 0xAB
 )
 
 func (m MundiClient) GetSystemTime() time.Time {
-	lsb, msb := calculateChecksum(getSystemTime, emptyLength)
-	message := []byte{startOfText, getSystemTime, emptyLength, lsb, msb, endOfTransmission}
-
-	systemTime := m.sendAndReceive(message)
-
-	lsbDate, msbDate := calculateChecksum(getSystemDate, emptyLength)
-	messageDate := []byte{startOfText, getSystemDate, emptyLength, lsbDate, msbDate, endOfTransmission}
-	systemDate := m.sendAndReceive(messageDate)
+	systemTime := m.getSystemTime()
+	systemDate := m.getSystemDate()
 
 	then, _ := time.Parse("02012006150405", string(systemDate[3:11])+string(systemTime[3:9]))
 
 	return then
+}
+
+func (m MundiClient) getSystemTime() []byte {
+	lsb, msb := calculateChecksum(getSystemTime, emptyLength)
+	message := []byte{startOfText, getSystemTime, emptyLength, lsb, msb, endOfTransmission}
+	return m.sendAndReceive(message)
+}
+
+func (m MundiClient) getSystemDate() []byte {
+	lsb, msb := calculateChecksum(getSystemDate, emptyLength)
+	message := []byte{startOfText, getSystemDate, emptyLength, lsb, msb, endOfTransmission}
+	return m.sendAndReceive(message)
 }
