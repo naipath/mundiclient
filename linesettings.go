90ed69521ca70b23382ae69c9ee136b4718b1a83
tvs <teije-vs@hotmail.com>
2017-07-22 13:49:26 +0200

    Implemented SetLineSettingsDelay

--- linesettings.go
+++ linesettings.go
@@ -4,6 +4,7 @@ import "encoding/binary"
 
 const (
 	getLineSettings = 0xA7
+	setLineSettings = 0xDD
 
 	Single1       = detectorType(0)
 	Single2       = detectorType(1)
@@ -32,5 +33,31 @@ func (m MundiClient) GetLineSettings() LineSettings {
 		response[4],
 		detectorType(response[6]),
 		binary.BigEndian.Uint16([]byte{response[9], response[8]}),
+	}
+}
+
+func (m MundiClient) SetLineSettingsDelay(delay uint16) {
+
+	var length byte = 0x03
+	var setLineSettingsDelayID byte = 0xC4
+	msbDelay, lsbDelay := byte(delay>>8), byte(delay&0xff)
+
+	lsb, msb := calculateChecksum(setLineSettings, length, setLineSettingsDelayID, lsbDelay, msbDelay)
+	message := []byte{startOfText, setLineSettings, length, setLineSettingsDelayID, lsbDelay, msbDelay, lsb, msb, endOfTransmission}
+
+	response := m.sendAndReceive(message)
+
+	if response[0] != acknowledge {
+		if response[0] == 0x15 {
+			switch response[1] {
+			case 0xC0:
+				panic("Product present not available with static marking")
+			case 0xC1:
+				panic("Tacho or max line speed required")
+			case 0xC2:
+				panic("Product interval too small")
+			}
+		}
+		panic("Could not set LineSettings")
 	}
 }
