5dceacbd74f1df266391446b63e9a631c4a0bad7
tvs <teije-vs@hotmail.com>
2017-07-23 15:09:11 +0200

    Implemented SelectMarkingFile

--- markingfile.go
+++ markingfile.go
@@ -1,9 +1,28 @@
 package mundiclient
 
 const (
+	selectMarkingFile     = 0x98
 	getCurrentMarkingFile = 0xDB
 	getMarkingFiles       = 0x99
 )
+
+func (m MundiClient) SelectMarkingFile(filename string) {
+	data := []byte(filename)
+	length := byte(len(data))
+
+	lsb, msb := calculateChecksum(append([]byte{selectMarkingFile, length}, data...)...)
+
+	startOfMessage := []byte{startOfText, selectMarkingFile, length}
+	endOfMessage := []byte{lsb, msb, endOfTransmission}
+
+	message := append(append(startOfMessage, data...), endOfMessage...)
+
+	response := m.sendAndReceive(message)
+
+	if response[0] != acknowledge {
+		panic("could not select marking file")
+	}
+}
 
 func (m MundiClient) GetCurrentMarkingFile() string {
 	lsb, msb := calculateChecksum(getCurrentMarkingFile, emptyLength)
